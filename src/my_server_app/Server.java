9aca76d2412168e4ec0637d132ca6c9b36be3d0b
devcrack <jaha.devcrack@gmail.com>
2018-01-29 16:00:53 -0600

    Casi terminando esta webbada
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.
    Basura

--- src/my_server_app/Server.java
+++ src/my_server_app/Server.java
@@ -56,12 +56,34 @@ public class Server extends Thread {
     }
     
     
-    
-    public void broadcast(String message, PrintWriter  out) {
+    /*Sending all to the client recently added...............................................................*/
+//    public void broadcast_users_names(String message, PrintWriter  out) {
+    public void broadcast_users_names(PrintWriter pr_client, String this_client_name) {
+        boolean append_name = true;
         
+        for(PrintWriter pw_client:  this.printWriter_list_clients) {
+            if(pr_client == pw_client)
+                append_name = false;
+            for(String str_client_name : this.name_list_clients) {                
+                if(!append_name && !this_client_name.equals(str_client_name)) {
+                    pw_client.println("$" + str_client_name);
+                    System.out.println(str_client_name + " is in list");//Printing in terminal 
+                }
+                
+//                    pw_client.println("$" + str_client_name);
+//                    System.out.println(str_client_name + " is in list");//Printing in terminal 
+//                }
+//                else 
+//                    if(!this_client_name.equals(str_client_name)) {
+//                        pw_client.println("$" + str_client_name);
+//                        System.out.println(str_client_name + " is in list");//Printing in terminal 
+//                    }
+            }
+        }
     } 
-    /*###############################<INNER CLASS>############################*/
     
+    
+    /*###############################<INNER CLASS>############################*/
     public class Read extends Thread {
         String client_name;
         String password;
@@ -83,6 +105,8 @@ public class Server extends Thread {
                 name_list_clients.add(client_name); //Adding the client name recenlty added.
                 System.out.println(client_name + " joined");//Printing in terminal 
                 /*Sending the list of clients to this client*/
+                broadcast_users_names(this.client_out, client_name);
+                
             }
             catch(Exception e) {
                 System.err.println("ERRORR!!!!" + e.getMessage());
